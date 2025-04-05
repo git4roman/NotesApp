3eff665a3f1c86fad3d0c3e2deacbba728053607
git4roman <romanforgit@gmail.com>
2025-04-05 08:18:18 +0545

    Setting Up models

---
+++ NotesApi/Models/Note.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotesApi.Models
+{
+    public class Note
+    {
+        public int Id { get; set};
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int UserId { get; set; };
+        public User? user { get; set; };
+    }
+}
